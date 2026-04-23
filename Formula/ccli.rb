5000451771f25da45e3811fd2fa32c20fc02cee1
Kory Hoopes <purchasing@seabear.dev>
2026-04-23 10:49:21 -0600

    Update stable channel to 1.13.0

--- Formula/ccli.rb
+++ Formula/ccli.rb
@@ -1,7 +1,7 @@
 class Ccli < Formula
   desc "Command-line information store for quick reference of frequently used data"
   homepage "https://github.com/seabearDEV/codexCLI"
-  version "1.12.2"
+  version "1.13.0"
   license "MIT"
 
   livecheck do
@@ -10,17 +10,17 @@ class Ccli < Formula
   end
 
   on_macos do
-    url "https://github.com/seabearDEV/codexCLI/releases/download/v1.12.2/ccli-macos-universal"
-    sha256 "1eed1da780fd3263e7e23a9b7a3966a08a91c854bdd63fd24de8a44a2bc33bd0"
+    url "https://github.com/seabearDEV/codexCLI/releases/download/v1.13.0/ccli-macos-universal"
+    sha256 "67a0b144c792fbea6427de009a83c656f0fe482d625710f097e4813615d6c3fd"
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/seabearDEV/codexCLI/releases/download/v1.12.2/ccli-linux-arm64"
-      sha256 "c0248983bff536b0b995d8661678debe8282f8b4af05e1e9db33e10911c37fc0"
+      url "https://github.com/seabearDEV/codexCLI/releases/download/v1.13.0/ccli-linux-arm64"
+      sha256 "9853e38887133f2bce9e3ff2af73394c4d349f75909d3b197805301c3aafcbad"
     else
-      url "https://github.com/seabearDEV/codexCLI/releases/download/v1.12.2/ccli-linux-x64"
-      sha256 "37633c0af207442ad90b644f1d18eadc0ff5c90495b4f8783cd8a6aeb6ceb6b1"
+      url "https://github.com/seabearDEV/codexCLI/releases/download/v1.13.0/ccli-linux-x64"
+      sha256 "d696af4f9b11e23d64c1ed4853e49c96c86b25d2c8d2dfb63b1bd7a91818c25f"
     end
   end
 
